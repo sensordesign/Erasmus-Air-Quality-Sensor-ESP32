1b43e71158edefbc541d3bbb371b732627a423fc
Kristian <k.buriasco.2636@vallauri.edu>
2023-01-06 15:31:48 +0100

    Revert "Delete credentials.cpp"
    This reverts commit 7a85ba022e44c045536007a1dce65cbbfdd039b4.

---
+++ Sensor/credentials.cpp
@@ -0,0 +1,22 @@
+#include "credentials.h"
+
+char *wifi_name() {
+  return "Infostrada-9398E1";
+}
+
+char *wifi_PWD() {
+  return "BDLMD8P73D";
+}
+
+char *iNFLUXDB_URL(){
+  return "https://europe-west1-1.gcp.cloud2.influxdata.com";
+}
+char *iNFLUXDB_TOKEN(){
+  return "sfxn7YKbhdghIoSXpakejuUUAXcBwfW-TNu1JEIT3wmeorda1BcXm_pZJW5DAXNKV7x_y23Irponc5lbW5k3JA==";
+}
+char *iNFLUXDB_ORG() {
+  return "8f7a0a2a53c35db0";
+}
+char *iNFLUXDB_BUCKET(){
+  return "sensor_data_test";
+}
