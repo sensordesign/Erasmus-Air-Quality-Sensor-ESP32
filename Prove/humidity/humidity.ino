9eff11a75cf2846c50516f1066a13ba6d9914e7c
Edoardo <103110362+edomazzone@users.noreply.github.com>
2023-01-12 20:09:01 +0100

    Update humidity.ino

--- Prove/humidity/humidity.ino
+++ Prove/humidity/humidity.ino
@@ -1,17 +1,32 @@
-#include <DHT.h>
+#include "DHT.h"
 #define DHTPIN 5
 #define DHTTYPE DHT11
-
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
 }
 
 void loop() {
-  double t = dht.readTemperature();
-  double h = dht.readHumidity();
-  Serial.print(t);
-  Serial.println(h);
   delay(5000);
+  double h = dht.readHumidity();
+  double t = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(F("Heat index: "));
+  Serial.print(hic);
+  Serial.println(F("°C "));
 }
