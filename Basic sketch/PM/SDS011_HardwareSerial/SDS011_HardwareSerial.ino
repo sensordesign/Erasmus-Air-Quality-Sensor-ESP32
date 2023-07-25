0b5f1c746960a41f14e90815ab983e5856540c4c
Edoardo <103110362+edomazzone@users.noreply.github.com>
2023-07-25 15:26:51 +0200

    updates

--- Basic sketch/PM/SDS011_HardwareSerial/SDS011_HardwareSerial.ino
+++ Basic sketch/PM/SDS011_HardwareSerial/SDS011_HardwareSerial.ino
@@ -15,16 +15,36 @@ SDS011 my_sds;
 HardwareSerial port(2);
 #endif
 
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+
+RTC_DATA_ATTR int bootCount = 0;
+
 void setup() {
-	my_sds.begin(&port);
-	Serial.begin(115200);
+  my_sds.begin(&port);
+  Serial.begin(115200);
 }
 
 void loop() {
-	err = my_sds.read(&p25, &p10);
-	if (!err) {
-		Serial.println("P2.5: " + String(p25));
-		Serial.println("P10:  " + String(p10));
-	}
-	delay(100);
+  my_sds.wakeup();
+  delay(5000); // working 5 seconds
+
+  err = my_sds.read(&p25, &p10);
+  if (!err) {
+    Serial.println("P2.5: " + String(p25));
+    Serial.println("P10:  " + String(p10));
+  }
+  my_sds.sleep();
+  ++bootCount;
+  Serial.println("Boot number: " + String(bootCount));
+
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+                 " Seconds");
+
+  Serial.println("Going to sleep now");
+  delay(1000);
+  Serial.flush();
+  esp_deep_sleep_start();
+
 }
